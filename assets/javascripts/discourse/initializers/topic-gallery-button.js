f2401e34a371214e3a548196f15215d8f4ed0d42
Don <71207900+VaperinaDEV@users.noreply.github.com>
2026-02-04 15:49:00 +0100

    Remove topic footer gallery button
    Removed gallery button configuration and related logic from the topic gallery button initializer.

--- assets/javascripts/discourse/initializers/topic-gallery-button.js
+++ assets/javascripts/discourse/initializers/topic-gallery-button.js
@@ -1,40 +1,11 @@
-import { getOwner } from "@ember/owner";
 import { withPluginApi } from "discourse/lib/plugin-api";
 import PostMenuGalleryButton from "../components/post-menu-gallery-button";
-
-const GALLERY_PRIORITY = 250;
-
-function galleryButtonConfig(anonymousOnly) {
-  return {
-    id: anonymousOnly ? "topic-gallery-anon" : "topic-gallery",
-    icon: "images",
-    priority: GALLERY_PRIORITY,
-    label: "discourse_topic_gallery.gallery_button_label",
-    title: "discourse_topic_gallery.gallery_button_title",
-    anonymousOnly,
-    action() {
-      const topic = this.topic;
-      const router = getOwner(this).lookup("service:router");
-      router.transitionTo("topicGallery", topic.slug, topic.id);
-    },
-    classNames: ["topic-gallery"],
-    dropdown() {
-      return this.site.mobileView;
-    },
-    displayed() {
-      return !this.site.mobileView && this.site.can_view_topic_gallery;
-    },
-  };
-}
 
 export default {
   name: "topic-gallery-button",
 
   initialize() {
     withPluginApi((api) => {
-      api.registerTopicFooterButton(galleryButtonConfig(false));
-      api.registerTopicFooterButton(galleryButtonConfig(true));
-
       const siteSettings = api.container.lookup("service:site-settings");
       if (siteSettings.topic_gallery_post_menu_button) {
         api.registerValueTransformer(
